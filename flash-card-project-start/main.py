fe94c3a3aac1b9b0f939accc8e3c75ae9f853e16
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-01-29 16:45:33 -0800

    day 31 python challenge falshcardacpstone project

---
+++ flash-card-project-start/main.py
@@ -0,0 +1,58 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv('data/french_words.csv')
+to_learn = data.to_dict(orient='records')
+current_card = {}
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(card_word, text=current_card['French'], fill='black')
+    canvas.itemconfig(canvas_image,image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text='English', fill="white")
+    canvas.itemconfig(card_word, text=current_card['English'], fill="white")
+    canvas.itemconfig(canvas_image, image=card_back_image)
+
+
+window = Tk()
+window.title('Flashy')
+window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file='images/card_front.png')
+card_back_image = PhotoImage(file='images/card_back.png')
+canvas_image = canvas.create_image(400, 263, image=card_front_image)
+card_title = canvas.create_text(400, 150, text='', font=('Ariel', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text='', font=('Ariel', 60,'bold'))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+correct_image = PhotoImage(file='images/right.png')
+correct_button = Button(image=correct_image, highlightthickness=0, command=next_card)
+correct_button.grid(row=1, column=1)
+
+wrong_image = PhotoImage(file='images/wrong.png')
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
+wrong_button.grid(row=1, column=0)
+
+next_card()
+
+
+
+
+
+
+
+
+window.mainloop()
