495a6b9581a887dbdb6fe497826ebc64ec7c476b
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-01-29 17:02:12 -0800

    day 31 python challenge falshcardacpstone project

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -3,10 +3,17 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv('data/french_words.csv')
-to_learn = data.to_dict(orient='records')
 current_card = {}
+to_learn={}
+
+try:
+    data = pandas.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    original_data = pandas.read_csv('data/french_words.csv')
+    to_learn = original_data.to_dict(orient='records')
+else:
+    to_learn = data.to_dict(orient='records')
+
 
 def next_card():
     global current_card, flip_timer
@@ -22,6 +29,13 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card['English'], fill="white")
     canvas.itemconfig(canvas_image, image=card_back_image)
 
+def is_known():
+    to_learn.remove(current_card)
+    # print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv('data/words_to_learn.csv', index=False)
+
+    next_card()
 
 window = Tk()
 window.title('Flashy')
@@ -39,7 +53,7 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 correct_image = PhotoImage(file='images/right.png')
-correct_button = Button(image=correct_image, highlightthickness=0, command=next_card)
+correct_button = Button(image=correct_image, highlightthickness=0, command=is_known)
 correct_button.grid(row=1, column=1)
 
 wrong_image = PhotoImage(file='images/wrong.png')
